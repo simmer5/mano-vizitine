269d8c28671d83acd64bf1398219049834659439
Mindaugas <strumke@yahoo.com>
2025-01-09 18:41:00 +0100

    pridejau pwa iconeles

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,12 +11,12 @@ export default function manifest(): MetadataRoute.Manifest {
 		theme_color: '#000000',
 		icons: [
 			{
-				src: '/icon-192x192.png',
+				src: '/cabinet-192.png',
 				sizes: '192x192',
 				type: 'image/png',
 			},
 			{
-				src: '/icon-512x512.png',
+				src: '/cabinet-512.png',
 				sizes: '512x512',
 				type: 'image/png',
 			},
